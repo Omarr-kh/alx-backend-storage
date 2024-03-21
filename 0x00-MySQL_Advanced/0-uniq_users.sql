bead3bcd6f29c797d179506f60ff85684bc19d50
Omarr-kh <omar1752003@gmail.com>
2024-03-21 19:32:33 +0000

    users table creation

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- creating users table
+-- id integer, never null, auto increment and primary key
+-- email string (255 characters), never null and unique
+-- name string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)
+);
