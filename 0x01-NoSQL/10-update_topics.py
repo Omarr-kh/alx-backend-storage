379837436dd19b21294ec43842b62e8e1bfeebff
omar <omar1752003@gmail.com>
2024-03-27 07:51:36 -0700

    added comment

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,5 +3,6 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """ update all with name=name """
     mongo_collection.update_many({"name": name},
                                  {"$set": {"topics": topics}})
